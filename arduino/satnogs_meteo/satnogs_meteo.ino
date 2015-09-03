db673e599b475e3cdf869c57b985a51ab3826f38
Marios Papachristou <papachristoumarios@gmail.com>
2015-09-03 14:08:22 +0300

    Update satnogs_meteo.ino

--- arduino/satnogs_meteo/satnogs_meteo.ino
+++ arduino/satnogs_meteo/satnogs_meteo.ino
@@ -8,59 +8,34 @@ dependencies: dht library & bmp085 library (https://github.com/adafruit/Adafruit
 
 */
 
-#define DHT22_PIN 9
-#define DELAY_INTERVAL 1000
+#include "DHT.h"
 
-#include <Wire.h>
-#include <Adafruit_BMP085.h>
-#include <dht.h>
+#define DHTPIN 8
+#define DHTTYPE DHT22
 
-dht DHT;
-bmp Adafruit_BMP085;
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("meteorological station initiated");
-  
-  if (!bmp.begin()) {
-   Serial.println)("could not initialize barometer BMP180");
-   while(1) {}
-  }
-  
-}
-
-void print_humidity_and_temperature() {
- int chk = DHT.read22(DHT22_PIN);
- switch(chk) 
- {
- //debug
-   case DHTLIB_OK:
-   break;
-  case DHTLIB_ERROR_CHECKSUM:
-   Serial.println("Checksum Error");
-   break;
-  case DHTLIB_ERROR_TIMEOUT:
-   Serial.println("Timeout Error");
-   break;
-  default:
-   Serial.println("Unknown Error");
-   break;   
- }
-  Serial.print(DHT.temperature);
-  Serial.println(" C");
-  delay(DELAY_INTERNVAL);
-  Serial.print(DHT.humidity);
-  Serial.println( " RH");
-}
-
-void print_pressure() {
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
- print_humidity_and_temperature();
- delay(DELAY_INTERVAL);
- print_pressure();
- delay(DELAY_INTERVAL);
+
+  float h = dht.readHumidity();
+
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  //as JSON
+  Serial.print(" { 'temp': '");
+  Serial.print(t);
+  Serial.print("', 'hum': '");
+  Serial.print(h);
+  Serial.println("' }");
+  delay(2000);
+  
 }
